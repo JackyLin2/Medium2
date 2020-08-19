1b4678a53f5e3f57e4a6a81ccee157bef3cf89d4
lucassherwin <lksherwin@gmail.com>
2020-08-19 13:53:33 -0400

    added login method to AuthController, also created AuthController

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,12 @@
+class AuthController < ActionController
+    def login
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            payload = {user_id: user.id}
+            token = encode_token(payload)
+            render json: {user: user, jwt: token, success: "Welcome back, #{user.username}"}
+        else
+            render json: {failure: "Log in failed! Username or password invalid!"}
+        end
+    end
+end
