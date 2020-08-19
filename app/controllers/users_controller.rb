8f5ed3d2a2854d059cf09b97a8789f8a25ddf0e5
lucassherwin <lksherwin@gmail.com>
2020-08-19 13:44:38 -0400

    added user signing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,8 +15,15 @@ class UsersController < ApplicationController
     end 
 
     def create 
-        @user = User.create(user_params)
-        render json: @user
+        @user = User.create(user_params) # user instance is created
+        if user.valid? # if it is valid
+            payload = {user_id: user.id} # payload object is created with the user instance's ID 
+            token = encode_token(payload) # payload is passed into the encode_token method which is defined in the ApplicationController
+            render json: {user: user, jwt: token}
+        else
+            render json: {errors: user.errors.full_messages}, status: :not_acceptable
+        end
+        # render json: @user
     end 
 
     def edit 
